b14325bc83f3a8a50a4324d2537db297450b3708
Fiorusss <giordano.fioretti1993@gmail.com>
2024-07-18 19:06:59 +0200

    portfolio finito

---
+++ script.js
@@ -0,0 +1,15 @@
+let togglebtn=document.querySelector(".togglebtn");
+let nav=document.querySelector(".navlinks");
+let links=document.querySelector(".navlinks li");
+
+togglebtn.addEventListener("click",function(){
+    this.classList.toggle("click");
+    nav.classList.toggle("open");
+})
+
+let typed=new Typed(".input", {
+    strings:["Frontend Developer", "Backend Developer", "Web Developer"],
+    typedSpeed:50,
+    backSpeed:40,
+    loop:true
+})
